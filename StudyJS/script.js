e768649eace02d3317909be9d13a06b95062a39a
Penny Hindle <penny.hindle@x-labsystems.co.uk>
2019-07-09 15:46:25 +0100

    Loops task 4

--- StudyJS/script.js
+++ StudyJS/script.js
@@ -1,21 +1,5 @@
-visitor = prompt("Enter Admin, other or cancel")
-
-if(visitor == "Cancel") {
-  alert("Cancelled");
-}
-else if(visitor == 'Admin') {
-  password = prompt("Please enter your password");
-
-  if (password == "Cancel") {
-
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 == 0) {
+    alert( i );
   }
-  if (password == "TheMaster") {
-    alert("Welcome!")
-  }
-  else {
-    alert("Wrong password");
-  }
-}
-else {
-  alert("I don't know you");
 }
