86e20c15ce260e1c2ca583c25a7d5f3e2e129e72
Penny Hindle <penny.hindle@x-labsystems.co.uk>
2019-07-09 14:40:04 +0100

    Conditional operators task 5

--- StudyJS/script.js
+++ StudyJS/script.js
@@ -1,5 +1,8 @@
-let a =1;
-let b = 2;
-let result = (a + b < 4) ? 'Below': 'Over';
+login = 'Employee';
 
-alert(result);
+let message = (login == 'Employee') ? 'Hello':
+              (login == 'Director') ? 'Greetings':
+              (login == '')         ? 'No login':
+              '';
+
+alert(message)
