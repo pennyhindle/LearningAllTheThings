ff4f9ab648d5e3cc1bb55055cb3ddf0704b5616d
Penny Hindle <penny.hindle@x-labsystems.co.uk>
2019-07-10 13:29:46 +0100

    Objects task 3

--- StudyJS/script.js
+++ StudyJS/script.js
@@ -1,6 +1,13 @@
-function isEmpty(obj) {
-  for (let key in obj) {
-    return false;
-  }
-  return true;
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
 }
+
+let sum = 0;
+
+for (let key in salaries) {
+  sum = sum + salaries[key];
+}
+
+alert(sum)
