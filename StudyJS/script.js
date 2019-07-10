14261e4094a486a8690a45dd786e16629368583d
Penny Hindle <penny.hindle@x-labsystems.co.uk>
2019-07-10 13:39:35 +0100

    Objects task 4

--- StudyJS/script.js
+++ StudyJS/script.js
@@ -1,13 +1,11 @@
-let salaries = {
-  John: 100,
-  Ann: 160,
-  Pete: 130
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+  for (let key in menu) {
+    obj[key] *= 2;
+  }
 }
-
-let sum = 0;
-
-for (let key in salaries) {
-  sum = sum + salaries[key];
-}
-
-alert(sum)
