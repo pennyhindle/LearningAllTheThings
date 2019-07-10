8dcd4a1cfdbc4fd3c07dd95aa0a61f33604c77ed
Penny Hindle <penny.hindle@x-labsystems.co.uk>
2019-07-10 11:09:02 +0100

    Functions task 2

--- StudyJS/script.js
+++ StudyJS/script.js
@@ -1,8 +1,11 @@
-function checkAge(age) {
-  return (age > 18) ? true: confirm('Did parents allow you? ')
-}
+let a = 5;
+let b = 7;
 
-
-function checkAge(age) {
-  return (age > 18) || confirm('Did parents allow you?');
+function min(a, b) {
+  if(a < b) {
+    return a;
+  }
+  else {
+    return b;
+  }
 }
