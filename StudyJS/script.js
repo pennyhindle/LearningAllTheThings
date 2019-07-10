ee23c192dd594ec8703683791ad774479f9e0ed8
Penny Hindle <penny.hindle@x-labsystems.co.uk>
2019-07-10 11:47:18 +0100

    Coding style task 1

--- StudyJS/script.js
+++ StudyJS/script.js
@@ -1,10 +1,19 @@
-function ask(question, yes, no) {
-  if (confirm(question)) yes()
-  else no();
+function pow(x, n) {
+  let result = 1;
+
+  for (let i = 0; i < n; i++) {
+    result*=x;
+  }
+
+  return result;
 }
 
-ask(
-  "Do you agree?",
-  () => alert("You agreed."),
-  () => alert("You canceled the execution.")
-);
+let x = prompt("x?",'');
+let n = prompt("n?",'');
+
+if (n <= 0) {
+  alert(`Power ${n} is not supported, please enter an integer number greater than zero`);
+}
+else {
+  alert(pow(x, n))
+}
