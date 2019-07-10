9c3dabf6856babcbf543b14c5cb942c44933e812
Penny Hindle <penny.hindle@x-labsystems.co.uk>
2019-07-10 10:56:20 +0100

    Loops task 7

--- StudyJS/script.js
+++ StudyJS/script.js
@@ -1,5 +1,10 @@
-let number = 0;
+number = prompt("Please enter a number");
 
-while(number < 100) {
-  number = prompt("enter a number");
+nextPrime:
+for (let i = 2; i <= number; i++) {
+
+  for (let divider = 2; divider < i; divider++) {
+    if(i % divider == 0)  continue nextPrime;
+  }
+  alert(i);
 }
