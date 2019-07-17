22d5c4c41c7158f716690581154ffa8aeccd2510
Penny Hindle <penny.hindle@x-labsystems.co.uk>
2019-07-17 13:54:20 +0100

    Created edit view

--- StudyMVC/Controllers/StudentController.cs
+++ StudyMVC/Controllers/StudentController.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StudyMVC.Models;
 
 namespace StudyMVC.Controllers
 {
     public class StudentController : Controller
     {
-        // GET: Student
-        public string Index()
+        IList<Student> studentList = new List<Student>() {
+                    new Student(){ StudentId=1, StudentName="John", Age = 18 },
+                    new Student(){ StudentId=2, StudentName="Steve", Age = 21 },
+                    new Student(){ StudentId=3, StudentName="Bill", Age = 25 },
+                    new Student(){ StudentId=4, StudentName="Ram", Age = 20 },
+                    new Student(){ StudentId=5, StudentName="Ron", Age = 31 },
+                    new Student(){ StudentId=6, StudentName="Chris", Age = 17 },
+                    new Student(){ StudentId=7, StudentName="Rob", Age = 19 }
+                };
+
+        public ActionResult Index()
         {
-            return "This is the Index action method";
+            return View(studentList);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            //Get the student from studentList sample collection for demo purpose.
+            //Get the student from the database in the real application
+            var std = studentList.Where(s => s.StudentId == Id).FirstOrDefault();
+
+            return View(std);
+        }
+        
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            return RedirectToAction("Index");
         }
     }
 }
