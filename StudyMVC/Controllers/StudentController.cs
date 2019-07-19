05bde98555b9262a6c513bd4685689c3c2b4b003
Penny Hindle <penny.hindle@x-labsystems.co.uk>
2019-07-19 09:52:22 +0100

    Fix edit student method

--- StudyMVC/Controllers/StudentController.cs
+++ StudyMVC/Controllers/StudentController.cs
@@ -9,7 +9,7 @@ namespace StudyMVC.Controllers
 {
     public class StudentController : Controller
     {
-        IList<Student> studentList = new List<Student>() {
+        static IList<Student> studentList = new List<Student>() {
                     new Student(){ StudentId=1, StudentName="John", Age = 18 },
                     new Student(){ StudentId=2, StudentName="Steve", Age = 21 },
                     new Student(){ StudentId=3, StudentName="Bill", Age = 25 },
@@ -19,11 +19,13 @@ namespace StudyMVC.Controllers
                     new Student(){ StudentId=7, StudentName="Rob", Age = 19 }
                 };
 
+
         public ActionResult Index()
         {
             return View(studentList);
         }
 
+        [HttpGet]
         public ActionResult Edit(int Id)
         {
             var std = studentList.Where(s => s.StudentId == Id).FirstOrDefault();
@@ -34,8 +36,12 @@ namespace StudyMVC.Controllers
         [HttpPost]
         public ActionResult Edit(Student std)
         {
-            if (ModelState.IsValid) // determins whether submitted values satisfy data annotation attributes
+            // check whether submitted values satisfy data annotation attributes
+            if (ModelState.IsValid) 
             {
+                var updateStudent = studentList.Where(s => s.StudentId == std.StudentId).FirstOrDefault().StudentId -1;
+                studentList[updateStudent].StudentName = std.StudentName;
+                studentList[updateStudent].Age = std.Age;
                 return RedirectToAction("Index");
             }
 
