e4dc9f24af76e5ebd56addd55bf18955de46e188
Penny Hindle <penny.hindle@x-labsystems.co.uk>
2019-07-22 10:20:38 +0100

    run migration

--- MigrationsDemo/Migrations/201907220917598_AddPostClass.cs
+++ MigrationsDemo/Migrations/201907220917598_AddPostClass.cs
@@ -10,23 +10,25 @@ namespace MigrationsDemo.Migrations
             CreateTable(
                 "dbo.Posts",
                 c => new
-                    {
-                        PostId = c.Int(nullable: false, identity: true),
-                        Title = c.String(maxLength: 200),
-                        Content = c.String(),
-                        BlogId = c.Int(nullable: false),
-                    })
+                {
+                    PostId = c.Int(nullable: false, identity: true),
+                    Title = c.String(maxLength: 200),
+                    Content = c.String(),
+                    BlogId = c.Int(nullable: false),
+                })
                 .PrimaryKey(t => t.PostId)
                 .ForeignKey("dbo.Blogs", t => t.BlogId, cascadeDelete: true)
-                .Index(t => t.BlogId);
-            
-            AddColumn("dbo.Blogs", "Rating", c => c.Int(nullable: false));
+                .Index(t => t.BlogId)
+                .Index(p => p.Title, unique: true);
+
+            AddColumn("dbo.Blogs", "Rating", c => c.Int(nullable: false, defaultValue: 3));
         }
-        
+
         public override void Down()
         {
-            DropForeignKey("dbo.Posts", "BlogId", "dbo.Blogs");
+            DropIndex("dbo.Posts", new[] { "Title" });
             DropIndex("dbo.Posts", new[] { "BlogId" });
+            DropForeignKey("dbo.Posts", "BlogId", "dbo.Blogs");
             DropColumn("dbo.Blogs", "Rating");
             DropTable("dbo.Posts");
         }
