2013bde117fdae03c916c8731fa0156e402a0325
Penny Hindle <penny.hindle@x-labsystems.co.uk>
2019-07-22 10:18:22 +0100

    Update model to include posts

--- MigrationsDemo/Model.cs
+++ MigrationsDemo/Model.cs
@@ -16,5 +16,20 @@ namespace MigrationsDemo
         public string Name { get; set; }
 
         public string Url { get; set; }
+
+        public int Rating { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        [MaxLength(200)]
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
     }
 }
